d90f0d2c90b175fa78855945fec5241e7238e925
unknown <kothuriphanindra1999@gmail.com>
2024-09-30 08:08:24 -0400

    Developed code for generating Excel

---
+++ src/edu/charlotte/GeneratingExcel.java
@@ -0,0 +1,82 @@
+package edu.charlotte;
+
+import java.util.*;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class GeneratingExcel {
+    public static void main(String[] args) {
+//        int[] arraySizesForExecution = new int[] {1000, 2000, 3000, 4000, 5000, 10000, 20000, 40000, 50000, 60000, 80000, 90000, 100000};
+        int[] arraySizesForExecution = new int[] {1000, 2000, 3000, 4000, 5000, 10000, 20000, 40000, 50000};
+//        int[] arraySizesForExecution = new int[]{1000, 2000};
+        HashMap<Integer, HashMap<Integer, List<Long>>> mapForGeneratingExcel = new HashMap<>();
+        Scanner scanningInput = new Scanner(System.in);
+        System.out.print("Enter the number of times you want to run the codes with different random inputs: ");
+        int numberOfTimesToRunTheCodes = scanningInput.nextInt();
+        scanningInput.close();
+
+        for (int sizeOfArrayExecuting : arraySizesForExecution) {
+            HashMap<Integer, List<Long>> mapForExecutionTime = new HashMap<>();
+            for (int numberOfTimesToRunTheCodesIndex = 0; numberOfTimesToRunTheCodesIndex < numberOfTimesToRunTheCodes; numberOfTimesToRunTheCodesIndex++) {
+                mapForExecutionTime.put(numberOfTimesToRunTheCodesIndex, UnSortedInput.mainForUnSortedInput(sizeOfArrayExecuting));
+            }
+            mapForGeneratingExcel.put(sizeOfArrayExecuting, mapForExecutionTime);
+        }
+
+        try {
+//        Workbook workbook = new XSSFWorkbook();
+            Workbook workbook = WorkbookFactory.create(false);
+            // Creating a new sheet
+            Sheet sheet = workbook.createSheet("UnSorting Input Data");
+
+            // Create header row
+            Row headerRow = sheet.createRow(0);
+            String[] headers = {"Size of the Array", "Number of Execution", "Insertion Sort", "Merge Sort", "In-Place Quick Sort", "Modified Quick Sort", "Heap Sort"};
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
+            }
+
+            int rowNumber = 1;
+            for (Map.Entry<Integer, HashMap<Integer, List<Long>>> rowData : mapForGeneratingExcel.entrySet()) {
+                Integer key = rowData.getKey();
+                HashMap<Integer, List<Long>> mapForExecutionTime = rowData.getValue();
+                for (Map.Entry<Integer, List<Long>> rowData1 : mapForExecutionTime.entrySet()) {
+                    Row row = sheet.createRow(rowNumber++);
+                    for (int columnNumber = 0; columnNumber < headers.length; columnNumber++) {
+                        Cell cell = row.createCell(columnNumber);
+                        if (columnNumber == 0 && key instanceof Integer) {
+                            cell.setCellValue(key);
+                        } else if (columnNumber == 1 && rowData1.getKey() instanceof Integer) {
+                            cell.setCellValue(rowData1.getKey());
+                        } else if (columnNumber > 1 && rowData1.getValue() instanceof List) {
+                            cell.setCellValue(rowData1.getValue().get(columnNumber - 2));
+                        }
+                    }
+                }
+            }
+
+            // Adjust column widths
+            for (int i = 0; i < headers.length; i++) {
+                sheet.autoSizeColumn(i);
+            }
+
+            // Write the output to a file
+            try (FileOutputStream fileOut = new FileOutputStream("UnSortedInput.xlsx")) {
+                workbook.write(fileOut);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Closing the workbook
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Excel file generated successfully!");
+    }
+}
+
--- src/edu/charlotte/UnSortedInput.java
+++ src/edu/charlotte/UnSortedInput.java
@@ -7,7 +7,9 @@ import edu.charlotte.sorting_techniques.MergeSort;
 import edu.charlotte.sorting_techniques.ModifiedQuickSort;
 import edu.charlotte.utils.SortingAlgorithmsUtilities;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -20,13 +22,83 @@ import java.util.Scanner;
  * It also measures whether each of the sort are working as expected by comparing the result with the result of the in-built sort method of the Arrays class in Java.
  */
 public class UnSortedInput {
-    public static void main(String[] args) {
-        long startTime, endTime, executionTime;
-        Scanner scanningInput = new Scanner(System.in);
-        System.out.print("Enter the number of inputs to be sorted: ");
-        int sizeOfArray = scanningInput.nextInt();
-        scanningInput.close();
+//    public static void main(String[] args) {
+//        long startTime, endTime, executionTime;
+//        Scanner scanningInput = new Scanner(System.in);
+//        System.out.print("Enter the number of inputs to be sorted: ");
+//        int sizeOfArray = scanningInput.nextInt();
+//        scanningInput.close();
+//
+//        int[] inputArray = new int[sizeOfArray];
+//        SortingAlgorithmsUtilities.generateRandomValues(inputArray);
+//
+//        int[] inputArrayForHeapSort = Arrays.copyOf(inputArray, sizeOfArray);
+//        int[] inputArrayForInsertionSort = Arrays.copyOf(inputArray, sizeOfArray);
+//        int[] inputArrayForMergeSort = Arrays.copyOf(inputArray, sizeOfArray);
+//        int[] inputArrayForInPlaceQuickSort = Arrays.copyOf(inputArray, sizeOfArray);
+//        int[] inputArrayForModifiedQuickSort = Arrays.copyOf(inputArray, sizeOfArray);
+//
+//        InsertionSort insertionSortObject = InsertionSort.getInstance();
+//        startTime = System.nanoTime();
+//        System.out.println("*** Insertion Sort ***");
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForInsertionSort));
+//        insertionSortObject.sort(inputArrayForInsertionSort);
+//        endTime = System.nanoTime();
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForInsertionSort));
+//        executionTime = endTime - startTime;
+//        System.out.println("The time taken to sort array using Insertion sort in nanoseconds is: " + executionTime);
+//        Arrays.sort(inputArray); //
+//        System.out.println("Checking the validity of Insertion Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForInsertionSort))); //
+//
+//        MergeSort mergeSortObject = MergeSort.getInstance();
+//        startTime = System.nanoTime();
+//        System.out.println("*** Merge Sort ***");
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForMergeSort));
+//        mergeSortObject.sort(inputArrayForMergeSort, 0, sizeOfArray - 1);
+//        endTime = System.nanoTime();
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForMergeSort));
+//        executionTime = endTime - startTime;
+//        System.out.println("The time taken to sort array using Merge sort in nanoseconds is: " + executionTime);
+//        System.out.println("Checking the validity of Merge Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForMergeSort))); //
+//
+//        InPlaceQuickSort inPlaceQuickSortObject = InPlaceQuickSort.getInstance();
+//        startTime = System.nanoTime();
+//        System.out.println("*** In-Place Quick Sort ***");
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForInPlaceQuickSort));
+//        inPlaceQuickSortObject.sort(inputArrayForInPlaceQuickSort, 0, sizeOfArray - 1);
+//        endTime = System.nanoTime();
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForInPlaceQuickSort));
+//        executionTime = endTime - startTime;
+//        System.out.println("The time taken to sort array using In-Place Quick sort in nanoseconds is: " + executionTime);
+//        System.out.println("Checking the validity of In-Place Quick Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForInPlaceQuickSort))); //
+//
+//        ModifiedQuickSort modifiedQuickSortObject = ModifiedQuickSort.getInstance();
+//        startTime = System.nanoTime();
+//        System.out.println("*** Modified Quick Sort ***");
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForModifiedQuickSort));
+//        modifiedQuickSortObject.sort(inputArrayForModifiedQuickSort, 0, sizeOfArray - 1);
+//        endTime = System.nanoTime();
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForModifiedQuickSort));
+//        executionTime = endTime - startTime;
+//        System.out.println("The time taken to sort array using Modified Quick sort in nanoseconds is: " + executionTime);
+//        System.out.println("Checking the validity of Modified Quick Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForModifiedQuickSort))); //
+//
+//        HeapSort HeapSortObject = HeapSort.getInstance();
+//        startTime = System.nanoTime();
+//        System.out.println("*** In-Place Heap Sort ***");
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForHeapSort));
+//        HeapSortObject.sort(inputArrayForHeapSort);
+//        endTime = System.nanoTime();
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForHeapSort));
+//        executionTime = endTime - startTime;
+//        System.out.println("The time taken to sort array using In-Place Heap sort in nanoseconds is: " + executionTime);
+//        System.out.println("Checking the validity of In-Place   Heap Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForHeapSort))); //
+//    }
 
+    public static List<Long> mainForUnSortedInput(int sizeOfArray) {
+        long startTime, endTime, executionTime;
+
+        List<Long> listForExecutionTime = new ArrayList<>();
         int[] inputArray = new int[sizeOfArray];
         SortingAlgorithmsUtilities.generateRandomValues(inputArray);
 
@@ -39,57 +111,64 @@ public class UnSortedInput {
         InsertionSort insertionSortObject = InsertionSort.getInstance();
         startTime = System.nanoTime();
         System.out.println("*** Insertion Sort ***");
-        System.out.println("Original array is: " + Arrays.toString(inputArrayForInsertionSort));
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForInsertionSort));
         insertionSortObject.sort(inputArrayForInsertionSort);
         endTime = System.nanoTime();
-        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForInsertionSort));
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForInsertionSort));
         executionTime = endTime - startTime;
         System.out.println("The time taken to sort array using Insertion sort in nanoseconds is: " + executionTime);
         Arrays.sort(inputArray); //
         System.out.println("Checking the validity of Insertion Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForInsertionSort))); //
+        listForExecutionTime.add(executionTime);
 
         MergeSort mergeSortObject = MergeSort.getInstance();
         startTime = System.nanoTime();
         System.out.println("*** Merge Sort ***");
-        System.out.println("Original array is: " + Arrays.toString(inputArrayForMergeSort));
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForMergeSort));
         mergeSortObject.sort(inputArrayForMergeSort, 0, sizeOfArray - 1);
         endTime = System.nanoTime();
-        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForMergeSort));
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForMergeSort));
         executionTime = endTime - startTime;
         System.out.println("The time taken to sort array using Merge sort in nanoseconds is: " + executionTime);
         System.out.println("Checking the validity of Merge Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForMergeSort))); //
+        listForExecutionTime.add(executionTime);
 
         InPlaceQuickSort inPlaceQuickSortObject = InPlaceQuickSort.getInstance();
         startTime = System.nanoTime();
         System.out.println("*** In-Place Quick Sort ***");
-        System.out.println("Original array is: " + Arrays.toString(inputArrayForInPlaceQuickSort));
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForInPlaceQuickSort));
         inPlaceQuickSortObject.sort(inputArrayForInPlaceQuickSort, 0, sizeOfArray - 1);
         endTime = System.nanoTime();
-        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForInPlaceQuickSort));
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForInPlaceQuickSort));
         executionTime = endTime - startTime;
         System.out.println("The time taken to sort array using In-Place Quick sort in nanoseconds is: " + executionTime);
         System.out.println("Checking the validity of In-Place Quick Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForInPlaceQuickSort))); //
+        listForExecutionTime.add(executionTime);
 
         ModifiedQuickSort modifiedQuickSortObject = ModifiedQuickSort.getInstance();
         startTime = System.nanoTime();
         System.out.println("*** Modified Quick Sort ***");
-        System.out.println("Original array is: " + Arrays.toString(inputArrayForModifiedQuickSort));
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForModifiedQuickSort));
         modifiedQuickSortObject.sort(inputArrayForModifiedQuickSort, 0, sizeOfArray - 1);
         endTime = System.nanoTime();
-        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForModifiedQuickSort));
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForModifiedQuickSort));
         executionTime = endTime - startTime;
         System.out.println("The time taken to sort array using Modified Quick sort in nanoseconds is: " + executionTime);
         System.out.println("Checking the validity of Modified Quick Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForModifiedQuickSort))); //
+        listForExecutionTime.add(executionTime);
 
         HeapSort HeapSortObject = HeapSort.getInstance();
         startTime = System.nanoTime();
         System.out.println("*** In-Place Heap Sort ***");
-        System.out.println("Original array is: " + Arrays.toString(inputArrayForHeapSort));
+//        System.out.println("Original array is: " + Arrays.toString(inputArrayForHeapSort));
         HeapSortObject.sort(inputArrayForHeapSort);
         endTime = System.nanoTime();
-        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForHeapSort));
+//        System.out.println("Array after sorting is: " + Arrays.toString(inputArrayForHeapSort));
         executionTime = endTime - startTime;
         System.out.println("The time taken to sort array using In-Place Heap sort in nanoseconds is: " + executionTime);
         System.out.println("Checking the validity of In-Place   Heap Sort: " + Arrays.toString(inputArray).equals(Arrays.toString(inputArrayForHeapSort))); //
+        listForExecutionTime.add(executionTime);
+
+        return listForExecutionTime;
     }
 }
